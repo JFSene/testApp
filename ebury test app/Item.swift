1ef01553db087672a00e5eb18ac9a50e0cda67d6
JFSene <joel.sene@hotmail.com>
2024-06-13 23:25:17 +0100

    Initial Commit

---
+++ ebury test app/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  ebury test app
+//
+//  Created by Joel Sene on 13/06/2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
